3c33b844b24602ef773db7f1ef244578effbbf74
rush-adin <fardin23105101353@diu.edu.bd>
2024-11-01 09:51:30 +0600

    ready made template

---
+++ src/main/java/com/ralfin/managementsystemrmg/ManagementSystemRMG.java
@@ -0,0 +1,118 @@
+package com.ralfin.managementsystemrmg;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+class Garment {
+
+    public String id;
+    public String name;
+    public String description;
+    public String size;
+    public String color;
+    public double price;
+    public int stockQuantity;
+
+    void updateStock(int quantity) {
+
+    }
+
+    double calculateDiscountPrice(double discountPercentage) {
+
+        return 0;
+    }
+}
+
+class Fabric {
+
+    public String id;
+    public String type;
+    public String color;
+    public double pricePerMeter;
+
+    public double calculateCost(double meters) {
+
+        return 0;
+    }
+}
+
+class Supplier {
+
+    public String id;
+    public String name;
+    public String contactInfo;
+    private List<Fabric> suppliedFabrics;
+
+    public void addFabric(Fabric fabric) {
+
+    }
+
+    public List<Fabric> getSuppliedFabrics() {
+
+        return suppliedFabrics;
+
+    }
+}
+
+class Order {
+
+    public String orderId;
+    public Date orderDate;
+    public List<Garment> garments;
+    public double totalAmount;
+
+    public void addGarment(Garment garment) {
+
+    }
+
+    public double calculateTotalAmount() {
+
+        return 0;
+    }
+
+    void printOrderDetails() {
+
+    }
+}
+
+class Customer {
+
+    public String customerId;
+    public String name;
+    public String email;
+    public String phone;
+
+    public void placeOrder(Order order) {
+
+    }
+
+    public List<Order> viewOrders() {
+        return null;
+    }
+}
+
+class Inventory {
+
+    public List<Garment> garments;
+
+    public void addGarment(Garment garment) {
+
+    }
+
+    public void removeGarment(String id) {
+
+    }
+
+    public Garment findGarment(String id) {
+
+        return null;
+    }
+}
+
+public class ManagementSystemRMG {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
