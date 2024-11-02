4b9b0484395154baf406e17dde3921f49a5a6bc2
rush-adin <fardin23105101353@diu.edu.bd>
2024-11-03 00:32:57 +0600

    updated methods

--- src/main/java/com/ralfin/managementsystemrmg/ManagementSystemRMG.java
+++ src/main/java/com/ralfin/managementsystemrmg/ManagementSystemRMG.java
@@ -1,5 +1,6 @@
-package com.ralfin.managementsystemrmg;
+//package com.ralfin.managementsystemrmg;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
@@ -15,12 +16,12 @@ class Garment {
     public int stockQuantity;
 
     void updateStock(int quantity) {
-
+        stockQuantity = quantity;
     }
 
     double calculateDiscountPrice(double discountPercentage) {
-
-        return 0;
+        double discount = price * (discountPercentage / 100);
+        return discount;
     }
 }
 
@@ -32,8 +33,8 @@ class Fabric {
     public double pricePerMeter;
 
     public double calculateCost(double meters) {
-
-        return 0;
+        double cost = pricePerMeter * meters;
+        return cost;
     }
 }
 
@@ -42,16 +43,14 @@ class Supplier {
     public String id;
     public String name;
     public String contactInfo;
-    private List<Fabric> suppliedFabrics;
+    private List<Fabric> suppliedFabrics = new ArrayList<>();
 
     public void addFabric(Fabric fabric) {
-
+        suppliedFabrics.add(fabric);
     }
 
     public List<Fabric> getSuppliedFabrics() {
-
         return suppliedFabrics;
-
     }
 }
 
@@ -59,20 +58,32 @@ class Order {
 
     public String orderId;
     public Date orderDate;
-    public List<Garment> garments;
+    public int customerID;
+    public List<Garment> garments = new ArrayList<>();
     public double totalAmount;
 
     public void addGarment(Garment garment) {
-
+        garments.add(garment);
     }
 
     public double calculateTotalAmount() {
-
-        return 0;
+        for (Garment g : garments) {
+            totalAmount += g.price;
+        }
+        return totalAmount;
     }
 
     void printOrderDetails() {
-
+        String line = "-".repeat(50);
+        System.out.println(line);
+        System.out.println("|" + " ".repeat(18) + "Order Details" + " ".repeat(17) + "|");
+        System.out.println(line);
+        for (Garment g : garments) {
+            System.out.printf("| %-12s : %-30s  |%n", "Name", g.name);
+            System.out.printf("| %-12s : %-31.2f |%n", "Price", g.price);
+            System.out.printf("| %-12s : %-30s  |%n", "Description", g.description);
+            System.out.println(line);
+        }
     }
 }
 
@@ -82,8 +93,12 @@ class Customer {
     public String name;
     public String email;
     public String phone;
+    private List<Order> orders = new ArrayList<>();
 
     public void placeOrder(Order order) {
+        orders.add(order);
+        order.printOrderDetails();
+        System.out.println("Order Placed");
 
     }
 
@@ -94,18 +109,22 @@ class Customer {
 
 class Inventory {
 
-    public List<Garment> garments;
+    List<Garment> garments = new ArrayList<>();
 
-    public void addGarment(Garment garment) {
-
+    void addGarment(Garment garment) {
+        garments.add(garment);
     }
 
-    public void removeGarment(String id) {
-
+    void removeGarment(String id) {
+        garments.remove(id);
     }
 
-    public Garment findGarment(String id) {
-
+    Garment findGarment(String id) {
+        for (Garment g : garments) {
+            if (g.id == id) {
+                return g;
+            }
+        }
         return null;
     }
 }
